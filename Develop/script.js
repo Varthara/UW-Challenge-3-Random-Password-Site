6e383f119e3347f7ea745a656e3ea0e003e76452
Zane Sharif <Zane.Sharif@gmail.com>
2022-06-27 20:29:38 -0700

    making progress. Have conditionals set for user input

--- Develop/script.js
+++ Develop/script.js
@@ -15,18 +15,49 @@ function writePassword() {
 
 function generatePassword() {
   // Ask for password length between 8 and 128
-  var passwordLength = window.prompt("How many characters (8-128) would you like your password to be?")
-  // Code adding lowercase letters as an option 
-  var lowerLetters = 'abcdefghijklmnopqrstuvwxyz'.split('')
-  // coce adding uppdercase letters as an options 
+// pcik a number equal to or greater than 128 
+
+  var passwordLength = parseInt(prompt("Enter desired password length"))
+  console.log(passwordLength)
+  
+    if (passwordLength > 128) {
+    return("Please enter a length between 8-128 characters")
+  }
+
+  if (passwordLength < 8) {
+    return("Please enter a length between 8-128 characters")
+  }
+
+
+  // Code adding lowercase letters as an option   
+  var lowerLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  // Confirmation that lower case should be included 
+  var lLetterConfirm = confirm("Do you wish to have your password contain lower case letters?")
+
+
+  // code adding uppdercase letters as an options 
   var upperLetters = [ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
   'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
   'Y', 'Z' ]
+
+  // Confirmation that upper case should be included
+  var uLetterConfirm = confirm("Do you wish to have your password contain lower case letters?")
+
+  // code adding special characters as an option
+  var specialChar = ['!','@','#','$','%','^','&','*','(',')']
   
+  // Confirmation that special characters should be included
+
+  var sLetterConfirm = confirm("Do you wish to have special characters included?")
+
+  console.log(sLetterConfirm)
+  console.log(uLetterConfirm)
+  console.log(lLetterConfirm)
 
 
-  // Section that is supposed to take window input length, apply it to upper letters just to test if letters from the array will display and it's linked. 
-  return( upperLetters*passwordLength)
+ 
+  
+  return(passwordLength)
 }
 
 // Add event listener to generate button
